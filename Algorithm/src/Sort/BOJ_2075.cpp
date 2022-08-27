e6b10ce0fb8be372c866aed7fac081e0982d9f86
DESKTOP-35MHNLS\User <pwnstjr123@naver.com>
2022-08-27 21:30:00 +0900

    N번째 큰 수

---
+++ Algorithm/src/Sort/BOJ_2075.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+bool compare(int a, int b);
+
+int main(void){
+    ios::sync_with_stdio(false);    
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+    int arr[N*N];
+
+    for(int i = 0; i < N*N; i++){
+        cin >> arr[i];
+    }
+    sort(arr, arr + N*N, compare);
+    cout << arr[1] << endl; 
+    return 0;
+}
+
+bool compare(int a, int b){
+    return a > b;
+}
