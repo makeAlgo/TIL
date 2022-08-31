cc27f81a73af30d12de4b9e78511e05fc6b52ea3
DESKTOP-35MHNLS\User <pwnstjr123@naver.com>
2022-08-31 16:51:22 +0900

    A와 B

---
+++ Algorithm/src/Greedy/BOJ_12904.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void){
+    string S, T;
+    cin >> S >> T;
+    int cnt = 0;
+    while(1){
+        int idx = T.size();
+
+        if(T.size() == S.size()){
+            if(T == S)
+                cout << '1' << endl;
+            else
+                cout << '0' << endl;
+            
+            break;
+        }
+        char tmp = T[idx-1];
+        T.pop_back();
+        if(tmp == 'B'){
+            reverse(T.begin(),T.end());
+        }   
+        cnt++;
+    }
+    cout << '\n' << cnt;
+    return 0;
+}
