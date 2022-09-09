402152089359a7262522549f75ce908c06c22fde
DESKTOP-35MHNLS\User <pwnstjr123@naver.com>
2022-09-09 19:57:00 +0900

    점수 집계

---
+++ Algorithm/src/Sort/BOJ_9076.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        vector<int> v(5);
+        for (int j = 0; j < 5; j++) cin >> v[j];
+
+        sort(v.begin(), v.end());
+
+        if (v[3] - v[1] >= 4) {
+            cout << "KIN\n";
+            continue;
+        }
+        int ssum = 0;
+        for (int j = 1; j < 4; j++) ssum += v[j];
+        cout << ssum << '\n';
+    }
+}
