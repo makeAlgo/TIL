dff608812a2ecb53f71a4c4dc9c34397983104bb
DESKTOP-35MHNLS\User <pwnstjr123@naver.com>
2022-09-13 21:04:53 +0900

    수 정렬하기4

---
+++ Algorithm/src/Sort/BOJ_11931.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void){
+    int N;
+    cin >> N;
+
+    vector<int> v(N);
+    for(int i = 0; i < N; i++){
+        cin >> v[i];
+    }
+    sort(v.begin(),v.end(),greater<int>());
+
+    for(int i = 0; i < N; i++)
+        cout << v[i] << '\n';
+
+    return 0;
+}
