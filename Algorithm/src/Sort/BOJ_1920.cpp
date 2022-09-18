f0085ca6d8ef4a58f689d943c67731e5be705fea
DESKTOP-35MHNLS\User <pwnstjr123@naver.com>
2022-09-18 16:04:25 +0900

    수 찾기

---
+++ Algorithm/src/Sort/BOJ_1920.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+int main(void){
+    int N;
+    cin >> N;
+    set<long long> check;
+    for(int i = 0; i < N; i++){
+        int num;
+        cin >> num;
+        check.insert(num);
+    }
+    int M;
+    cin >> M;
+    for(int i = 0; i < M; i++){
+        int target;
+        cin >> target;
+        cout << check.count(target) << '\n';
+    }
+    return 0;
+}
