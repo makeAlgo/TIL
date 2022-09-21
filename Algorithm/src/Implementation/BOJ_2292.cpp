7c6e8be102f2d6f874586b2c95254b900b72452f
DESKTOP-35MHNLS\User <pwnstjr123@naver.com>
2022-09-21 11:14:43 +0900

    벌집

---
+++ Algorithm/src/Implementation/BOJ_2292.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num;
+    cin >> num;
+
+    int i = 0;
+    for(int sum = 2; sum <= num; i++)
+        sum += 6*i;
+    if(num == 1) 
+        i=1;
+
+    cout << i;
+    return 0;
+}
