1e7a340852383dd7db3c58f23f193ae714fb6a61
DESKTOP-35MHNLS\User <pwnstjr123@naver.com>
2022-10-13 16:02:22 +0900

    올바른 괄호

---
+++ Algorithm/src/Programmers/올바른괄호.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+bool solution(string s){
+    int cnt = 0;
+    for(int i = 0, len = s.size(); i < len; i++){
+        if(s[i] == '(') cnt++;
+        else cnt--;
+        
+        if(cnt < 0) return false;
+    }
+    
+    return cnt == 0 ? true : false;
+}
