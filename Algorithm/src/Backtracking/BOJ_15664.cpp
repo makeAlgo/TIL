275c41908f0a1ea6955212cf187733b9394bf1d0
DESKTOP-35MHNLS\User <pwnstjr123@naver.com>
2022-10-14 18:25:00 +0900

    N과 M (10)

---
+++ Algorithm/src/Backtracking/BOJ_15664.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int arr[10];
+vector<pair<int,bool>> v;
+
+int compare(pair<int,bool> p1, pair<int,bool> p2){
+    return p1.first < p2.first;
+}
+
+void func(int num, int k){ //현재까지 k개 선택
+    if(k == M){ //k개 선택했을 때
+        for(int i = 0; i < M; i++)
+            cout << arr[i] << ' ';
+        cout << '\n';
+        return;
+    }
+    int prev_num = -1;
+    for(int i = num; i < N; i++){
+        if(!v[i].second && prev_num != v[i].first){
+            v[i].second = true;
+            arr[k] = v[i].first;
+            prev_num = v[i].first;
+            func(i+1,k+1);
+            v[i].second = false;
+        }
+    }
+}
+int main(void){
+    //수열 => back tracking
+    cin >> N >> M;
+    for(int i = 0; i < N; i++){
+        int tmp;
+        cin >> tmp;
+        v.push_back(make_pair(tmp,false));
+    }
+    sort(v.begin(),v.end(),compare);
+    func(0,0);
+    return 0;
+}
