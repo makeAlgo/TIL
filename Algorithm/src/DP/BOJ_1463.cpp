1cc38d81ceaf7692158c74866efa697f3aa398d9
DESKTOP-35MHNLS\User <pwnstjr123@naver.com>
2022-10-17 16:19:57 +0900

    1로 만들기

---
+++ Algorithm/src/DP/BOJ_1463.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int d[1000001];
+int N;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N;
+    for(int i = 2; i <= N; i++){
+        d[i] = d[i-1] + 1;
+        if(i%2 == 0) d[i] = min(d[i],d[i/2]+1);
+        if(i%3 == 0) d[i] = min(d[i],d[i/3]+1);
+    }
+    cout << d[N];
+    return 0;
+}
