d248d911abf30e7738ab6547388d34839696a0cf
DESKTOP-35MHNLS\User <pwnstjr123@naver.com>
2022-10-26 15:07:44 +0900

    없는 숫자 더하기

---
+++ Algorithm/src/Programmers/없는숫자더하기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int sum = 0;
+    
+    for(int i = 0, len = numbers.size(); i < len; i++)
+        sum += numbers[i];
+    
+    return 45 - sum;
+}
