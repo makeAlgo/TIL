51afe2461241fadbb1d9d23e0cfd27fbcb943bcc
DESKTOP-35MHNLS\User <pwnstjr123@naver.com>
2022-11-18 13:23:25 +0900

    정수 삼각형

---
+++ Algorithm/src/DP/BOJ_1932.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[505][505];
+int n;
+
+int main(void){	
+	cin >> n;
+	
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < i+1; j++)
+			cin >> arr[i][j];
+	
+	for(int i = n-1; i >= 1; i--)
+		for(int j = 0; j < i; j++)
+			arr[i-1][j] += max(arr[i][j],arr[i][j+1]);
+	
+	cout << arr[0][0];
+	
+	return 0;
+}
