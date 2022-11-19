58cd6dcb506c8d893b1eb07058b1b7a016042a44
DESKTOP-35MHNLS\User <pwnstjr123@naver.com>
2022-11-19 14:59:34 +0900

    퇴사

---
+++ Algorithm/src/DP/BOJ_14501.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int dp[16];
+int pay[16];
+int time[16];
+int n;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> n;
+	for(int i = 1; i <= n; i++) {
+		cin >> time[i] >> pay[i];
+		for(int j = 0; j < i; j++) 
+			if(time[j] + j <= i && i+time[i]<=n+1)
+				dp[i] = max(dp[i], dp[j]+pay[i]);
+	}
+	int answer = 0;
+	for(int i = 0; i <= n; i++)
+		answer = max(answer, dp[i]);
+
+	cout << answer;
+	return 0;
+}
